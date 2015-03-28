37f6a14440cd11722db41b5697dbc33d3a2430ca
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-03-28 23:35:48 +0530

    test functions

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -2,6 +2,10 @@ package com.example
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+
+    println(Functions.circleArea(20))
+    println(Functions.fives(5,50))
+    println(Functions.diff2DPts((3,5),(1,7)))
+    println(Functions.tupleWithStrRep(true,22.5,"yes"))
   }
 }
